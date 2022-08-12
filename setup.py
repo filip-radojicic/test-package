129f06b126e4bc0e990477c7791ae152583290a5
Filip Radojicic <filip@mile.dev>
2022-08-12 14:09:25 +0200

    fix3

--- setup.py
+++ setup.py
@@ -1,8 +1,13 @@
+import email
 from setuptools import setup
 
 setup(
     name="anthem-utils",
     version="0.0.1",
+    description="Helper for handling anthem errors.",
+    long_description="Helper   for   handling   anthem   errors.",
+    author="Filip Radojicic",
+    author_email="filip.radojicic.ue@gmail.com",
     py_modules=["anthemutils.py"],
     package_dir={'':'src'}
 )
